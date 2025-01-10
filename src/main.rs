beb27ae58f242a8a2169036cd8d9561ca09eec9b
aminwhat <m.aminwhat@gmail.com>
2025-01-10 17:38:31 +0330

    use queries

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,40 @@
 #![allow(unused)]
 
-use axum::{response::Html, routing::get, Router};
-use std::net::SocketAddr;
+use axum::{
+    extract::Query,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use serde::Deserialize;
+use std::{fmt::format, net::SocketAddr};
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(|| async { Html("Hello <strong>World!</strong>") }),
-    );
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
 
+    // region: -- Start Server --
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING on {addr}\n");
     axum::Server::bind(&addr)
         .serve(routes_hello.into_make_service())
         .await
         .unwrap();
+    // endregion: -- Start Server --
 }
+
+// region: -- Handler Hello
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World!");
+
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+// endregion: -- Handler Hello
